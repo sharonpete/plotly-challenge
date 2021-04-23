e961d9e58acddd274ca843cb191dd613acc9a1f1
Sharon Templin <sharon@hollygo.es>
2021-04-23 14:45:16 -0500

    Implemented function for bargraph

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,45 @@ console.log('app.js loaded');
 function drawBargraph (sampleId) {
     console.log(`drawBargraph(${sampleId})`);
 
+    // read in the data 
+    d3.json("data/samples.json").then(data => {
+        //console.log(data);
+
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        console.log(resultArray);
+
+        var result = resultArray[0];
+
+        console.log(result);
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        console.log(otu_ids);
+        console.log(otu_labels);
+    
+        var sample_values = result.sample_values;
+        console.log(sample_values);
+
+        yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse(); //TBD
+
+        var barData = {
+            x: sample_values.slice(0,10).reverse(), // tbd
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0,10).reverse(),  //tbd
+            orientation: "h"
+        }
+
+        var barArray = [barData];
+        var barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: {t: 30, l: 150}
+        }
+
+        Plotly.newPlot("bar", barArray, barLayout);
+
+    });
 
 }
 
@@ -44,24 +83,18 @@ function initDashboard() {
 
         var id = sampleNames[0];
 
+        // Draw the graphs and the metadata
         drawBargraph(id);
 
         drawBubblechart(id);
 
         showMetadata(id);
 
-        drawgauge(id);
+        //drawgauge(id);
 
 
     });
 
-
-    // Update the bar graph
-    
-
-    // Update the bubblechart
-
-    // Update the demographic information
 }
 
 
