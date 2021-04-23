4c4bd48cfe3e84b89571e21bc6fa71e94cef6f3f
Sharon Templin <sharon@hollygo.es>
2021-04-23 14:09:53 -0500

    Populated the dropdown with sample ids

--- static/js/app.js
+++ static/js/app.js
@@ -10,9 +10,15 @@ function initDashboard() {
     // Populate the dropdown
     var selector = d3.select("#selDataset");
 
-    d3.json("data/samples.json").then(function(data) {
+    d3.json("data/samples.json").then(data => {
         console.log(data);
 
+        var sampleNames = data.names;
+        sampleNames.forEach(sampleId => {
+            selector.append("option")
+                .text(sampleId)
+                .property("value", sampleId);
+        });
     });
 
 
